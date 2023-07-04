7cff1a461cc61f32ab943425eb7690cd1c9d3b27
nzibukaemmanuel <84917246+nzibukaemmanuel@users.noreply.github.com>
2023-07-04 21:34:02 +0200

    Delete event-management-project-rwamugema-emmanuel-9c26c14a184db0f7b25a5e6fb897135ffc0bf108 directory

--- event-management-project-rwamugema-emmanuel-9c26c14a184db0f7b25a5e6fb897135ffc0bf108/event_manager/event_management/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Speaker
-
-class SpeakerForm(forms.ModelForm):
-    class Meta:
-        model = Speaker
-        fields = '__all__'
--- event-management-project-rwamugema-emmanuel-9c26c14a184db0f7b25a5e6fb897135ffc0bf108/event_manager/event_management/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import create_speaker, speaker_list
-
-app_name = 'event_management'
-
-urlpatterns = [
-    path('create_speaker/', create_speaker, name='create_speaker'),
-    path('speaker_list', speaker_list, name='speaker_list'),
-]
